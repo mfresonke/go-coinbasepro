3dafa2b308920f2811f1175f17027806bc1265b2
Max Fresonke <max@fresonke.com>
2020-05-20 10:03:15 -0400

    Add ability to deposit funds from a bank account

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/preichenberger/go-coinbasepro/v2
 
+go 1.14
+
 require github.com/gorilla/websocket v1.4.0
--- transfer.go
+++ transfer.go
@@ -17,3 +17,34 @@ func (c *Client) CreateTransfer(newTransfer *Transfer) (Transfer, error) {
 	_, err := c.Request("POST", url, newTransfer, &savedTransfer)
 	return savedTransfer, err
 }
+
+type PaymentMethod struct {
+	ID       string `json:"id"`
+	Type     string `json:"type"`
+	Name     string `json:"name"`
+	Currency string `json:"currency"`
+}
+
+func (c *Client) PaymentMethods() ([]PaymentMethod, error) {
+	var paymentMethods []PaymentMethod
+
+	url := fmt.Sprintf("/payment-methods")
+	_, err := c.Request("GET", url, nil, &paymentMethods)
+	return paymentMethods, err
+}
+
+type PaymentMethodDeposit struct {
+	Amount          string `json:"amount"`
+	Currency        string `json:"currency"`
+	PaymentMethodID string `json:"payment_method_id"`
+	// Response  Fields
+	ID string `json:"id,omitempty"`
+}
+
+func (c *Client) PaymentMethodDeposit(deposit *PaymentMethodDeposit) (PaymentMethodDeposit, error) {
+	var savedDeposit PaymentMethodDeposit
+
+	url := fmt.Sprintf("/deposits/payment-method")
+	_, err := c.Request("POST", url, deposit, &savedDeposit)
+	return savedDeposit, err
+}
